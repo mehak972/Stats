a43f1df4838a0386bd112244ee25dfb77be6fbf1
unknown <mehak530@gmail.com>
2023-05-17 00:26:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,92 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import Questions from './data.json';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import { Tooltip } from '@mui/material';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+ const[currentQuestion,setCurrentQuestion]=useState(0)
+ const maxQuestions=3;
+ const marks = [
+  {value: 0, label: 'Strongly Disagree'},
+  {value: 25, label: 'Disagree'},
+  {value: 50,label: 'Neutral'},
+  {value: 75,label: 'Agree'},
+  {value:100,label:'Strongly Agree'}
+];
+
+ const changeQuestion=()=>{
+  
+  if (currentQuestion<Questions.length-1){
+    setCurrentQuestion(currentQuestion+1)
+  }
+  else {}
+ }
+
+ const previousQuestion = () => {
+  if (currentQuestion > 0) {
+    setCurrentQuestion(currentQuestion - 1);
+  }else{}
+};
+
+ const valuetext=(value)=> {
+  return `${value}`;
+}
+
+const valueLabelFormat=(value)=> {
+  return marks.findIndex((mark) => mark.value === value) + 1;
+}
+const ValueLabelComponent = (props) => {
+  const { children, open, value } = props;
+    return (
+        <Tooltip open={open} enterTouchDelay={0} placement="bottom" title={`${value}%`}>
+          {children}
+        </Tooltip>
+    );
+}
+
+return (
+    <div className="container">
+      <div id="home" className="flex-center flex-column">
+      <h1>Personality Test</h1>
+      <div id="Qnumber">
+        <div id="Qnumber-item">
+        <p  id="progressText" className='Qnumber-prefix'>
+          Idealistic {currentQuestion+1}/{Questions.length}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div id="progressBar">
+        <div id='progressBarFull' style={{width:`${((currentQuestion+1)/maxQuestions)*100}%`}}></div>
+        </div>
+      
+        </div>
+      </div>
+      
+      <h2 id="question">{Questions[currentQuestion].question}</h2> 
+
+      <Box sx={{ width:"80rem" }}>
+         <Slider
+        aria-label="Restricted values"
+        defaultValue={0}
+        valueLabelFormat={valueLabelFormat}
+        ValueLabelComponent={ValueLabelComponent}
+        getAriaValueText={valuetext}
+        step={null}
+        onChange={changeQuestion}
+        marks={marks}
+      />
+    </Box>
+ 
+    <div className="button-container">
+          <button className="button1" onClick={previousQuestion}>
+            Previous
+          </button>
+          <button className="button2" onClick={changeQuestion}>
+            Next
+          </button>
+        </div>
+       </div>
+     </div>
   );
 }
 
